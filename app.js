deb50cd657cad12c7d070dba0d328fc7ea2f5d05
Sara Olveira <saralouro7@gmail.com>
2024-11-16 11:21:38 +0100

    Añadir mostrar ficha pokemon

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const search = document.querySelector("#search")
 let searchValue = null
 
 const sugerenciasPokemons = document.querySelector("#pokemons")
+const pokemonCard = document.querySelector("#card")
 
 let singlePokemon = null
 let pokemonMatches = null
@@ -36,21 +37,72 @@ const searchPokemons = async () => {
         } else if (pokemonMatches.length > 1) {
             showPokemons()
         }
-
-    }) 
+    })
 }
 
-const showPokemon = () => {
-    console.log(singlePokemon)
-    // Aqui hay que añadir para que pinte en el html la carta de un pokemon
+const showPokemon = async () => {
+    // Obtener la data del pokemon
+    const pokemonData = await getPokemons(singlePokemon[0].url)
+
+    // Visibilidad de los contenedores
+    sugerenciasPokemons.style.display = "none"
+    pokemonCard.style.display = "block"
+
+    // Mostrar información del pokemon en el HTML
+    pokemonCard.innerHTML = `<h2 class="pokemon-name">
+    ${pokemonData.name}</h2>
+    <div class="pokemon-imgs">
+        <img src="${
+            pokemonData.sprites.other.dream_world.front_default
+        }" alt="${pokemonData.name} frontal"/>
+         <img src="${pokemonData.sprites.back_default}" alt="${
+        pokemonData.name
+    } posterior"/>
+         <div class="pokemon-data">
+<p class="altura">Height: ${pokemonData.height} </p>
+<p class="altura">Weight: ${pokemonData.weight} </p>
+         </div>
+        <div class="pokemon-stats">
+<p class="health">${pokemonData.stats[0].stat.name}: ${
+        pokemonData.stats[0].base_stat
+    } </p>
+<p class="attack">${pokemonData.stats[1].stat.name}: ${
+        pokemonData.stats[1].base_stat
+    } </p>
+<p class="defense">${pokemonData.stats[2].stat.name}: ${
+        pokemonData.stats[2].base_stat
+    } </p>
+<p class="speed">${pokemonData.stats[5].stat.name}: ${
+        pokemonData.stats[5].base_stat
+    } </p>
+         </div>
+           <div class="pokemon-types">
+           <h3>Types</h3>
+           ${pokemonData.types
+               .map((type) => `<p>${type.type.name}</p>`)
+               .join("")}
+           </div>
+    </div>
+    
+    `
 }
 
 const showPokemons = () => {
-    console.log(pokemonMatches)
-    const pokemonsHTML = pokemonMatches.map((pokemon) => `<li>${pokemon.name}</li>`).join('')
-    sugerenciasPokemons.innerHTML = pokemonsHTML
+    // Visibilidad de los contenedores
+    sugerenciasPokemons.style.display = "block"
+    pokemonCard.style.display = "none"
 
+    // Mostrar los pokemons en el HTML
+    const pokemonsHTML = pokemonMatches
+        .map(
+            (pokemon) =>
+                `<li class="pokemon-name">${pokemon.name.replaceAll(
+                    "-",
+                    " "
+                )}</li>`
+        )
+        .join("")
+    sugerenciasPokemons.innerHTML = pokemonsHTML
 }
 
 searchPokemons()
-
