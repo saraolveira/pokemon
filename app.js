525733d2c0d0dd8d6690482f33e5caf3a3605382
EsterCastarnado <castarnado@hotmail.com>
2024-11-20 16:35:41 +0100

    Poner iconos cambiantes del modo noche

--- app.js
+++ app.js
@@ -357,6 +357,12 @@ document.querySelector("#modonoche .boton").addEventListener("click", () => {
     document.getElementById("card").classList.toggle("modo-oscuro")
     document.querySelector("footer").classList.toggle("modo-oscuro")
     document.querySelector(".frik").classList.toggle("modo-oscuro")
+    const Imagendelboton = document.getElementById("sol");
+    if(Imagendelboton.src.includes("sol.png")){
+    Imagendelboton.src = "./img/luna.png";}
+    else {
+    Imagendelboton.src = "./img/sol.png";
+    };
 })
 
 document.querySelector("#modonoche .boton").addEventListener("dblclick", () => {
